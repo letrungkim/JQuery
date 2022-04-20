2d207a435a09dbca109308b1aceba7b4dcf81f9b
letrungkim <s3634824@rmit.edu.vn>
2022-04-20 16:27:46 +0700

    1

--- index2/js/move.js
+++ index2/js/move.js
@@ -2,7 +2,7 @@ var totalImg = document.getElementsByClassName('slider--inside');
 var img = document.getElementsByClassName('img');
 var imgIndex = 0;
 var c, val;
-var arr = ['1', '2', '3'];
+var flag = true;
 
 
 $(document).ready(function () {
@@ -12,42 +12,58 @@ $(document).ready(function () {
 })
 
 function prevBtn() {
-    //imgIndex = 1;
     $('.prev').on('click', function () {
-        //imgIndex++;
-        // if (imgIndex == 1) {
-        //     $(`[imgIndex]:nth-child(${3})`).css({ "transform": `translateX(${100}%)`, "opacity": "" })
-        // } else {
+        if (flag == true) {
+            // Set flag, khi flag = true, thi function prev moi chay, nguoc lai flag = false thi ko
+            flag = false;
+            // Set flag = false, khi nay bam nut Prev thi function prev se ko hoat dong do function chi hoat dong khi flag = true
 
-        // }
-        $(`[imgIndex=${imgIndex + 1}]`).animate({ left: "100%", opacity: "0" }, 1000, function () {
-            $(`[imgIndex=${imgIndex}]`).animate({ "left": '-100%', "opacity": "0" }, 500);
-            $(`[imgIndex=${imgIndex + 1}]`).animate({ "left": '0%', "opacity": "1" }, 1000);
-        });
-
+            $('.slider--init img').css({ "opacity": "1" });
+            if (imgIndex < $('.slider--inside img').length - 1) {
+                $(`[imgIndex=${imgIndex + 1}]`).css({ "left": "100%" });
+                $(`[imgIndex=${imgIndex}]`).animate({ "left": "-=100%" });
+                $(`[imgIndex=${imgIndex + 1}]`).animate({ "left": "-=100%" }, function () {
+                    imgIndex++;
+                    flag = true;
+                    // Sau khi function chay hoan tat thi set flag = true de chay hinh khi an nut Prev, luc nay flag == true nen function moi co the chay tiep
+                });
+            } else {
+                $(`[imgIndex=${0}]`).css({ "left": "100%" });
+                $(`[imgIndex=${imgIndex}]`).animate({ "left": "-=100%" });
+                $(`[imgIndex=${0}]`).animate({ "left": "-=100%" }, function () {
+                    imgIndex = 0;
+                    flag = true;
+                });
+            }
+        }
     })
 }
 
 function nextBtn() {
-    //imgIndex = 1;
-
+    var nextIndex = $('.slider--inside img').length - 1;
     $('.next').on('click', function () {
-        //imgArr(100);
-        console.log(imgIndex)
-        //imgIndex++;
-        // if (imgIndex > 3) {
-        //     $(`[imgIndex]:nth-child(${3})`).css({ "transform": `translateX(${100}%)`, "opacity": "0" })
-        //     $(`[imgIndex]:nth-child(${2})`).css({ "transform": `translateX(-${100}%)`, "opacity": "0" })
-        //     $(`[imgIndex]:nth-child(${1})`).css({ "transform": `translateX(${0}%)`, "opacity": "1" })
-        //     imgIndex = 1;
-        // } else {
-        //     $(`[imgIndex]:nth-child(${3})`).css({ "transform": `translateX(-${100}%)`, "opacity": "0" })
-        //     $(`[imgIndex]:nth-child(${imgIndex - 2})`).css({ "opacity": "0", "transform": `translateX(-${100}%)` })
-        //     $(`[imgIndex]:nth-child(${imgIndex - 1})`).css({ "transform": `translateX(${100}%)`, "opacity": "0" })
-        //     $(`[imgIndex]:nth-child(${imgIndex})`).css({ "transform": `translateX(${0}%)`, "opacity": "1" })
-        // }
-        console.log($(`[imgIndex=${imgIndex + 1}]`))
-        $(`[imgIndex=${imgIndex + 1}]`).css({ "transform": `translateX(${0}%)`, "opacity": "1" });
+        if (flag == true) {
+            flag = false;
+            $('.slider--init img').css({ "opacity": "1" });
+            if (imgIndex == 0) {
+                $(`[imgIndex=${nextIndex}]`).css({ "left": "-100%" });
+                $(`[imgIndex=${imgIndex}]`).animate({ "left": "+=100%" });
+                $(`[imgIndex=${nextIndex}]`).animate({ "left": "+=100%" }, function () {
+                    imgIndex = nextIndex;
+                    flag = true;
+                });
+            } else {
+                $(`[imgIndex=${imgIndex - 1}]`).css({ "left": "-100%" });
+                // Cho hinh so 2 vi tri left = -100%
+                $(`[imgIndex=${imgIndex}]`).animate({ "left": "+=100%" });
+                // Animate hinh hien tai (hinh so 3) sang phai voi left += 100% -> left: 100%
+                $(`[imgIndex=${imgIndex - 1}]`).animate({ "left": "+=100%" }, function () {
+                    imgIndex--;
+                    flag = true;
+                });
+                // Animate hinh so 2 sang phai voi left += 100% -> left: 0
+            }
+        }
     })
 }
 
@@ -55,7 +71,6 @@ function nextBtn() {
 function imgArr(val) {
     $('.slider--inside img').each(function (index) {
         $(this).attr("imgIndex", index);
-
         if (index != 0) {
             $(this).css({ "left": `-100%` })
         }
